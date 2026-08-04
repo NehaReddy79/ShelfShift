a5b1175b0b0a65c4bd359428e0793517ee37b63c
Neha Reddy <nehareddy200709@gmail.com>
2026-08-04 17:14:24 +0530

    Add real conversion task, ignore test files and storage

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,5 +1,16 @@
 from app.celery_app import celery_app
+import subprocess
+
 
 @celery_app.task
 def add(x,y):
     return x + y
+
+@celery_app.task
+def convert_file_task(input_path , output_path):
+    try:
+        process = subprocess.run(['ebook-convert' , input_path , output_path] , check = True, capture_output=True , text=True)
+        return {"success" : True, "output_path" : output_path}
+    except subprocess.CalledProcessError as e : 
+        return {"success" : False, "Error" : e.stderr}
+    

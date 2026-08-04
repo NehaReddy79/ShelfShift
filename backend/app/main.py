0e2c3e2571fc4dab3f4f82e03528da804d2bbc3a
Neha Reddy <nehareddy200709@gmail.com>
2026-08-04 18:54:10 +0530

    Add file upload endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , UploadFile , File , HTTPException
+import os
 
 app = FastAPI()
+os.makedirs("storage/uploads/", exist_ok=True)
 
-# @app.get("/")
-# def root():
-#     return {"message" : "Working"}
+@app.get("/")
+def root():
+    return {"message" : "Working"}
 
-@app.get("/health")
-def health():
-    return {"status" : "ok"}
+@app.post("/convert")
+async def file_upload(file : UploadFile = File(...)):
+    if not file.filename :
+        raise HTTPException(status_code=400 , detail = "No file found")
+    
+    filepath = os.path.join("storage/uploads", file.filename)
+
+    contents = await file.read()
+
+    with open(filepath , "wb") as buffer : 
+        buffer.write(contents)
+
+    return {
+        "filename" : file.filename,
+        "size" : len(contents)
+    }

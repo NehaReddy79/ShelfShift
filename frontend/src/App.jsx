b941cfc16fbe16a1e43f3ab13bf32c92ba730bcc
Neha Reddy <nehareddy200709@gmail.com>
2026-08-11 12:14:40 +0530

    Adding frontend , and connecting to backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+import api from "./api"
+
+
+function App() {
+  const [file , setFile] = useState(null)
+  const [targetFormat , setTargetFormat] = useState("pdf")
+  const [result , setResult] = useState(null)
+  const [loading , setLoading] = useState(false)
+
+  function handleFileChange(e){
+    setFile(e.target.files[0])
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    if(!file) return 
+
+    setLoading(true)
+
+    const formData = new FormData();
+    formData.append("file" , file)
+    formData.append("target_format" , targetFormat)
+
+    try{
+      const resp = await api.post("/convert" , formData)
+      setResult(resp.data)
+    }catch(err){
+      console.error(err)
+      alert(err.response?.data?.detail || "Upload failed")
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange}></input>
+
+        <select value={targetFormat} onChange={(e) => setTargetFormat(e.target.value)}>
+          <option value="pdf">PDF</option>
+          <option value="epub">EPUB</option>
+          <option value="mobi">MOBI</option>
+          <option value="txt">TXT</option>
+        </select>
+
+        <button type="submit" disabled={loading}> 
+          {loading ? 'Converting..' : 'Convert'}
+        </button>
+      </form>
+
+      {result && (
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+      )}
+    </>
+  )
+}
+
+export default App
